9a0af031d2fddc49a8dd2594b3803088ac14e81b
chuongtang <chuong.tang@aol.com>
2021-11-17 11:46:07 -0700

    successful render waved addresses

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,11 @@ const App = () => {
   const [mined, setMined] = useState(false);
   const [count, setCount] = useState("0");
   const [loading, setLoading] = useState(false);
+  const [allWaves, setAllWaves] = useState([]);
 
   // This is where Waveportal smart contract deployed 🢛
-  const contractAddress = '0xB4d437c69a0A8b7Dd72063Ff3021E428E56726DD';
-  const contractABI=abiFile.abi;
+  const contractAddress = '0x232671ffc45d156D7deEC631fc5c43d08a8e2596';
+  const contractABI = abiFile.abi;
 
 
   const checkIfWalletIsConnected = async () => {
@@ -31,14 +32,15 @@ const App = () => {
       } else {
         console.log("We have the ethereum object", ethereum);
       }
+
       /*Check if we're authorized to access the user's wallet.
       Once we have access to this, we can call our smart contract*/
-
       const accounts = await ethereum.request({ method: 'eth_accounts' });
       if (accounts.length !== 0) {
         const account = accounts[0];
         console.log("Found an authorized account:", account);
-        setCurrentAccount(account)
+        setCurrentAccount(account);
+        getAllWaves();
       } else {
         console.log("No authorized account found")
       }
@@ -79,15 +81,15 @@ const App = () => {
         // Signer is an abstraction of an Ethereum Account
         const signer = provider.getSigner();
         const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
-        
-        
+
+
         // Here we call "getTotalWaves" function 🠋 in our smart contract
         let count = await wavePortalContract.getTotalWaves();
-        
+
         console.log("Retrieved total wave count...", count.toNumber());
 
         /* Execute the actual wave from your smart contract */
-        const waveTxn = await wavePortalContract.wave();
+        const waveTxn = await wavePortalContract.wave("Message from wave");
         console.log("Mining...", waveTxn.hash);
         console.log("HereIsWaveTxn BEFORE.", waveTxn);
 
@@ -106,11 +108,43 @@ const App = () => {
     } catch (error) {
       console.log(error)
     }
-}
+  }
+
+  // gets all waves from smart contract
+  const getAllWaves = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        // Call the getAllWaves method from the Smart Contract
+        const waves = await wavePortalContract.getAllWaves();
+
+
+        let wavesCleaned = [];
+        waves.forEach(wave => {
+          wavesCleaned.push({
+            address: wave.waver,
+            timestamp: new Date(wave.timestamp * 1000),
+            message: wave.message
+          });
+        });
+
+        setAllWaves(wavesCleaned);
+      } else {
+        console.log("Ethereum object doesn't exist!")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
 
   useEffect(() => {
     checkIfWalletIsConnected();
- 
+
   }, [])
 
 
@@ -124,7 +158,7 @@ const App = () => {
 
         <div className="bio">
           <h3>I'm Chuong, <a href="https://chuongtang.pages.dev" target="_blank" rel="noopener noreferrer">a web3.0 explorer.</a></h3>
-          <h4>Thanks for visit. Please send me a wave 👋</h4>
+          <h4>Thanks for your visit. Please send me a wave 👋</h4>
         </div>
 
         <button className="waveButton" onClick={wave}>
@@ -137,9 +171,18 @@ const App = () => {
           </button>
         )}
 
-      {mined && <div className="bio">Thank-you 🎉! Total Waves I have so far: {count}</div>}
-      {loading && <Loading/>}
-   
+        {mined && <div className="bio">Thank-you 🎉! Total Waves I have so far: {count}</div>}
+        {loading && <Loading />}
+
+        {allWaves.map((wave, index) => {
+          return (
+            <div key={index} style={{ backgroundColor: "OldLace", marginTop: "16px", padding: "8px" }}>
+              <div>Address: {wave.address}</div>
+              <div>Time: {wave.timestamp.toString()}</div>
+              <div>Message: {wave.message}</div>
+            </div>)
+        })}
+
       </div>
       <footer><p>Happily built with</p> <a href="https://buildspace.so/"> <img src="https://crypto-analysis.pages.dev/logos/buildspace.png" alt="buildspace logo" /></a></footer>
     </div>
